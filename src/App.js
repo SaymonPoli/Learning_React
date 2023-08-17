dd7fe97536d28c17552606643560aba475e69f24
SaymonPoli <99759719+SaymonPoli@users.noreply.github.com>
2023-08-17 11:00:14 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Salve família</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <ul>
+        <li>bão fi?</li>
+        <li>bão d+</li>
+      </ul>
+    </div>
+  );
+}
